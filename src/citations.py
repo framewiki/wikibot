ddc15076b06eda2e4fa173c0ffdf37999ffe2566
Josh Levin <morpheus636@morpheus636.com>
2025-01-08 23:51:43 +0000

    setup logging

--- src/citations.py
+++ src/citations.py
@@ -1,9 +1,13 @@
+import logging
 import re
 import requests
 from pathlib import Path
 
 import bs4
 import markdown
+
+logger = logging.getLogger(__name__)
+
 
 def check_citations(page: Path) -> None:
     # Read markdown, strip frontmatter, convert to html for parsing.
@@ -13,28 +17,37 @@ def check_citations(page: Path) -> None:
 
     # Loop over every footnote.
     doc = bs4.BeautifulSoup(contents, features="html.parser")
-    footnotes = doc.find("div", class_="footnote").find_all("li")
-    for note in footnotes:
+    footnotes = doc.find("div", class_="footnote")
+    if not footnotes:
+        logger.info(f"No footnotes found in {page.name}")
+        return
+    for note in footnotes.find_all("li"):
         links = note.find_all("a", href=lambda href: href and href.startswith(("http://", "https://")))
 
         # Make sure there is at least one link.
         if len(links) == 0:
-            print(f"WARN: Footnote in {page.name} has no links. Skipping.")
+            logger.warning(f"Footnote in {page.name} has no links. Skipping.")
             continue
-
+        
+        url = links[0].get("href")
+        logger.debug(f"Checking citation {url}")
+    
         # Check if there is an archive link.
         archive_link_present = False
         for link in links:
             if link.get("href").startswith("https://web.archive.org"):
                 archive_link_present = True # If there is an archive link, the footnote is fine.
+                logger.debug(f"Found archive link in citation {url}")
                 break
         if archive_link_present:
             continue
         
+        logger.debug(f"No archive link found in citation {url}. Attempting fix.")
+
         # Check if the link is broken. If not, create a new archive.
-        url = links[0].get("href")
         if requests.get(url).ok:
             archive_url = requests.get(f"https://web.archive.org/save/{url}", timeout=600).url
+            logger.info(f"Created new archive link for {url}")
 
         # If the link is broken, check if there is an existing archive.
         else:
@@ -43,8 +56,9 @@ def check_citations(page: Path) -> None:
             closest = snapshots.get("closest")
             if closest and closest["available"] is True:
                 archive_url = closest["url"]
+                logger.info(f"Found existing archive link for {url}")
             else:
-                print(f"WARN: Footnote in {page.name} contains broken link to {url}. No archived copy is available.")
+                logger.warning(f"Footnote in {page.name} contains broken link to {url}. No archived copy is available.")
                 continue
         
         # Put archive_url on the page.
--- src/wikibot.py
+++ src/wikibot.py
@@ -1,20 +1,28 @@
+import logging
 import os
 from pathlib import Path
 
 import citations
 
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
     # Get all markdown files from workspace.
     if os.environ.get("PRODUCTION"):
         workspace = "/github/workspace"
+        logger.debug("Using production workspace")
     else:
         workspace = "./test_wiki"
+        logger.debug("Using development workspace")
         
     repo = Path(workspace)
     pages = list(repo.glob("**/*.md"))
     
     for page in pages:
+        logger.info(f"Processing {page.name}")
         citations.check_citations(page)
 
 
